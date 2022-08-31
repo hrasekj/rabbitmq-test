f494d52b73bc2f772b26dbad60539e9740a4c7f6
Jakub Hrášek <hrasekj@gmail.com>
2022-08-31 14:30:00 +0200

    chore: rework createConnection to scale with channel per connection limit

--- src/mq/createConnection.ts
+++ src/mq/createConnection.ts
@@ -1,29 +1,65 @@
-import amqp from 'amqp-connection-manager';
+import amqp, { AmqpConnectionManager } from 'amqp-connection-manager';
+import { nanoid } from 'nanoid';
 import { urlToOrigin } from '../helpers/urlToOrigin';
 import config from '../config';
 import logger from '../logger';
 
-export const createConnection = () => {
+const connRegistry = new Map<string, string[]>();
+const connPool = new Map<string, AmqpConnectionManager>();
+const maxChannelCountPerConnection = 9;
+
+export const createConnection = (type: string) => {
+  const registry = connRegistry.get(type) || [];
+
+  for (let i = 0; i < registry.length; i++) {
+    const connId = registry[i];
+    const conn = connPool.get(connId);
+
+    // limit number of channels per connection
+    if (conn.channelCount < maxChannelCountPerConnection) {
+      return conn;
+    }
+  }
+
+  // create/register new connection
+  const newIndex = nanoid(8);
+  connPool.set(newIndex, newConnection(newIndex, type));
+  connRegistry.set(type, registry.concat(newIndex));
+
+  return connPool.get(newIndex);
+};
+
+const newConnection = (connId: string, type: string) => {
   // configure connection
   const conn = amqp.connect(config.queue, {
     heartbeatIntervalInSeconds: 10,
   });
 
-  // listen to connection events
+  // cleanup on disconnect
+  conn.once('disconnect', () => {
+    const registry = connRegistry.get(type);
+    connRegistry.set(
+      type,
+      registry.filter((val) => val === connId)
+    );
+    connPool.delete(connId);
+  });
+
+  // log connection events
   conn.on('connect', ({ url }) => {
-    logger.info("amqp connected to '%s'!", urlToOrigin(url));
+    logger.info("amqp(%s) connected to '%s'!", connId, urlToOrigin(url));
   });
   conn.on('connectFailed', ({ err, url }) => {
-    logger.error("amqp connect '%s' failed!", urlToOrigin(url), err);
+    logger.error("amqp(%s) connect '%s' failed!", connId, urlToOrigin(url), err);
   });
   conn.on('blocked', ({ reason }) => {
-    logger.info('amqp blocked %s!', reason);
+    logger.info('amqp(%s) blocked %s!', connId, reason);
   });
   conn.on('unblocked', () => {
-    logger.info('amqp unblocked!');
+    logger.info('amqp(%s) unblocked!', connId);
   });
   conn.on('disconnect', ({ err }) => {
-    logger.error('amqp disconnect!', err);
+    logger.error('amqp(%s) disconnect!', connId, err);
   });
 
   return conn;
